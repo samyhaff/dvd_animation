1fe8f371e93f6193df5aed24f80159f92d4cf4a9
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-03-30 19:39:58 +0200

    loop and closing event

--- main.c
+++ main.c
@@ -10,15 +10,42 @@ int main(int argc, char *argv[])
     window = SDL_CreateWindow("DVD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    SDL_RenderClear(renderer);
+    int done = 0;
+    SDL_Event event;
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect rect = {220, 140, 200, 200};
-    SDL_RenderFillRect(renderer, &rect);
+    while (!done)
+    {
+        while (SDL_PollEvent(&event)) 
+        {
+            switch (event.type) 
+            {
+                case SDL_QUIT:
+                    done = 1;
+                break;
+                case SDL_KEYDOWN:
+                {
+                    switch (event.key.keysym.sym)
+                    {
+                        case SDLK_ESCAPE:
+                            done = 1;
+                        break;
+                    }
+                }
+            }
+        }
 
-    SDL_RenderPresent(renderer);
-    SDL_Delay(2000);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_Rect rect = {220, 140, 200, 200};
+        SDL_RenderFillRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
+                    
+        SDL_Delay(100);
+    }
 
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
