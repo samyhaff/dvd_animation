b6d1114ae5b228ef7af1feb523dab40ff003cc05
Samy Haffoudhi <haffoudhisamy@gmail.com>
2021-03-30 20:10:34 +0200

    cleaned up code

--- main.c
+++ main.c
@@ -1,4 +1,28 @@
 #include <SDL2/SDL.h>
+#include "constants.h"
+
+int processQuit(SDL_Event event) 
+{
+    while (SDL_PollEvent(&event)) 
+    {
+        switch (event.type) 
+        {
+            case SDL_QUIT:
+                return 1;
+            break;
+            case SDL_KEYDOWN:
+            {
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:
+                        return 1;
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -7,7 +31,7 @@ int main(int argc, char *argv[])
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    window = SDL_CreateWindow("DVD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    window = SDL_CreateWindow("DVD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     int done = 0;
@@ -15,31 +39,13 @@ int main(int argc, char *argv[])
 
     while (!done)
     {
-        while (SDL_PollEvent(&event)) 
-        {
-            switch (event.type) 
-            {
-                case SDL_QUIT:
-                    done = 1;
-                break;
-                case SDL_KEYDOWN:
-                {
-                    switch (event.key.keysym.sym)
-                    {
-                        case SDLK_ESCAPE:
-                            done = 1;
-                        break;
-                    }
-                }
-            }
-        }
+        done = processQuit(event);
 
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_Rect rect = {220, 140, 200, 200};
+        SDL_Rect rect = {(WIDTH - DVD_WIDTH) / 2  , (HEIGHT - DVD_HEIGHT) / 2, 100, 50};
         SDL_RenderFillRect(renderer, &rect);
 
         SDL_RenderPresent(renderer);
